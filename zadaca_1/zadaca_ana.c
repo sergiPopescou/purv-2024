c042b5cbb8a54c8b42f495314eda7e2e1651b7ae
av <ana.vulin@student.etf.unibl.org>
2024-04-11 00:19:44 +0200

    gdb,log fajl

---
+++ zadaca_1/zadaca_ana.c
@@ -0,0 +1,11 @@
+ #include<stdio.h> 
+ #include<stdlib.h> 
+ #include<string.h> 
+  
+ int main(void) 
+ { 
+     char *strng ; 
+     strcat(strng, " dodatni strng"); 
+     printf("\n %s \n", strng); 
+     return 0; 
+ } 
