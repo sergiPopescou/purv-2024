43047e78a91ba59bb3a4267f566edd9eda5dee7f
Luka Zeljko <lukazeljko2002@gmail.com>
2024-10-11 08:13:32 +0200

    Added semaphores_exit and removed sleep in threads

--- Lab-3/src/conway.c
+++ Lab-3/src/conway.c
@@ -16,7 +16,8 @@ int matrix[10][10] = {{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
 				      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
 int pom_matrix[10][10];
-sem_t semaphores[100];
+sem_t semaphores_enter[28];
+sem_t semaphores_exit[28];
 pthread_mutex_t mutex;
 int sleep_usec = 300000; /* default sleep time between prints in microseconds */
 int done_cells[28] ={0};
@@ -30,10 +31,13 @@ int main(int argc, char *argv[]) {
 	int i,j;
 	if(argc>1)
 		sleep_usec = atoi(argv[1]);
-	for(i=0;i<100;i++) {
-		sem_init(&semaphores[i], 0, 0);
+	for(i=0;i<28;i++) {
+		sem_init(&semaphores_enter[i], 0, 0);
 	}
-	sem_post(&semaphores[0]);
+	for(i=0;i<28;i++) {
+		sem_init(&semaphores_exit[i], 0, 0);
+	}
+	sem_post(&semaphores_enter[0]);
 	pthread_t cells[100];
 	pthread_mutex_init(&mutex, NULL);
 	int args[100];
@@ -50,8 +54,11 @@ int main(int argc, char *argv[]) {
 		for(j=0;j<10;j++)
 			pthread_join(cells[i*10+j], NULL);
 
-	for(i=0;i<100;i++)
-		sem_destroy(&semaphores[i]);
+	for(i=0;i<28;i++)
+		sem_destroy(&semaphores_enter[i]);
+	pthread_mutex_destroy(&mutex);
+	for(i=0;i<28;i++)
+		sem_destroy(&semaphores_exit[i]);
 	pthread_mutex_destroy(&mutex);
 
 	return 0;
@@ -63,11 +70,19 @@ void* cell_func(void* arg) {
 	int i = ((*(int*)arg)/10)%10; // row
 	int j = (*(int*)arg)%10; // col
 	
+	int number_of_posts_prev = find_number_of_posts(priority-1);
 	int number_of_posts_this = find_number_of_posts(priority);
 	int number_of_posts_next = find_number_of_posts(priority+1);
 
 	while(1) {
-		sem_wait(&semaphores[priority-1]); // TODO [promijeni]
+		sem_wait(&semaphores_enter[priority-1]);
+		pthread_mutex_lock(&mutex);
+			if(priority != 1 && done_cells[priority-2] == number_of_posts_prev) {
+				for(k=0;k<number_of_posts_prev;k++)
+					sem_post(&semaphores_exit[priority-2]);
+				done_cells[priority-2] = 0;
+			}
+		pthread_mutex_unlock(&mutex);
 		int new_state = find_new_state(i, j);
 		pom_matrix[i][j] = new_state;
 		if(priority == 28) {
@@ -80,17 +95,17 @@ void* cell_func(void* arg) {
 			print_matrix(matrix);
 			fflush(stdout);
 			usleep(sleep_usec);
-			sem_post(&semaphores[0]);
+			sem_post(&semaphores_enter[0]);
 			continue;
 		}
 		pthread_mutex_lock(&mutex);
 			if(++done_cells[priority-1] == number_of_posts_this) {
 				for(k=0;k<number_of_posts_next;k++) {
-					sem_post(&semaphores[priority]);
+					sem_post(&semaphores_enter[priority]);
 				}
 			}
 		pthread_mutex_unlock(&mutex);
-		usleep(500); 
+		sem_wait(&semaphores_exit[priority-1]);
 	}
 	return 0;
 }
