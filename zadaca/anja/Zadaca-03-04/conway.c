616a0887d38cc7658dfbeb7e5d1847a8f75cd918
Anja <anja.djakovic@student.etf.unibl.org>
2025-01-31 23:18:27 +0100

    Zadaca 03-04: Conway Game Of Life-final

--- zadaca/anja/Zadaca-03-04/conway.c
+++ zadaca/anja/Zadaca-03-04/conway.c
@@ -7,68 +7,80 @@
 #define ALIVE 1
 #define DEAD 0
 
-// Struktura koja opisuje svaku celiju
 typedef struct {
     int x, y;
     int current_state;
     int next_state;
 } Cell;
 
-// Tabela igre
 Cell grid[SIZE][SIZE];
 
 // Barijera za sinhronizaciju niti
 pthread_barrier_t barrier; 
+
+int iterations = 20;
 
 // Funkcija za brojanje živih suseda
 int count_alive_neighbors(int x, int y) 
 {
     int alive_neighbors = 0;
 	
-    for (int i = -1; i <= 1; i++) {
-        for (int j = -1; j <= 1; j++) {
-			
-			/* Ne racunamo celije koje izlaze van opsega table i samu celiju (x,y)*/
-            if ( (x+i)<0 || (x+i)>=SIZE || (y+j)<0 || (y+j)>=SIZE || ( j==0 && i==0 ) ) continue;
-			
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+        
             int nx = x + i;
             int ny = y + j;
 			
-            if (grid[nx][ny].current_state == ALIVE) {
+			/* Ne racunamo celije koje izlaze van opsega table i samu celiju (x,y)*/
+            if ( nx<0 || nx>=SIZE || ny<0 || ny>=SIZE || ( j==0 && i==0 ) ) continue;
+			
+            if (grid[nx][ny].current_state == ALIVE) 
                 alive_neighbors++;
-            }
         }
     }
     return alive_neighbors;
 }
 
 void print_grid();
-// Inicijalizacija tabele sa slučajnim stanjima
+
 void initialize_grid();
 
 // Funkcija koju će svaka nit (ćelija) izvršavati
-void* cell_routine(void* arg) {
+void* cell_routine(void* arg)
+{
     Cell* cell = (Cell*)arg;
 
-    while (1) {
+    while (1)
+    {
 
+        if (iterations <= 0)
+        {
+            pthread_cancel(pthread_self());  
+            return NULL; 
+        }
+        
         int alive_neighbors = count_alive_neighbors(cell->x, cell->y);
 
-        if (cell->current_state == ALIVE) {
+        if (cell->current_state == ALIVE)
+        {
 			// Živa ćelija sa manje od 2 živa suseda ili vise od 3 ziva susjeda umire
-            if (alive_neighbors < 2 || alive_neighbors > 3) {
+            if (alive_neighbors < 2 || alive_neighbors > 3)
                 cell->next_state = DEAD;
-		    // Živa ćelija sa 2 ili 3 živa suseda preživljava.
-            } else if (alive_neighbors == 2 || alive_neighbors == 3){
+		    
+            // Živa ćelija sa 2 ili 3 živa suseda preživljava.
+            else if (alive_neighbors == 2 || alive_neighbors == 3)
                 cell->next_state = ALIVE;
-            }
-        } else {
+            
+        }
+        else
+        {
 			// Mrtva ćelija sa tačno 3 živa suseda oživljava
-            if (alive_neighbors == 3) {
+            if (alive_neighbors == 3)
                 cell->next_state = ALIVE;
-            } else {
+            else
                 cell->next_state = DEAD;
-            }
         }
 
         // Sinhronizacija sa drugim ćelijama
@@ -78,6 +90,9 @@ void* cell_routine(void* arg) {
         cell->current_state = cell->next_state;
 	
         // Sinhronizacija pre sledećeg ciklusa
+        pthread_barrier_wait(&barrier);
+        
+        // Cekanje na azururanje iterations varijable
         pthread_barrier_wait(&barrier);
     }
 
@@ -100,17 +115,18 @@ int main()
 	print_grid();
 	sleep(5); 
 
-    // Kreiranje niti za svaku ćeliju
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            pthread_create(&threads[i][j], NULL, cell_routine, (void*)&grid[i][j]);
-        }
+        for (int j = 0; j < SIZE; j++) 
+            pthread_create(&(threads[i][j]), NULL, cell_routine, (void*)&(grid[i][j]));
     }
 
     // Glavna nit koja kontroliše ispis i pauzu
-    while (1) {
+    while (iterations > 0)
+    {
         pthread_barrier_wait(&barrier); // Čeka da sve niti završe izračunavanje sledećeg stanja
         pthread_barrier_wait(&barrier); // Čeka da se niti sinhronizuju pre sledećeg ciklusa
+        iterations--;
+        pthread_barrier_wait(&barrier); 
 		print_grid();
         sleep(5); 
     }
@@ -118,6 +134,11 @@ int main()
     // Uništavanje barijere (iako je ovde nepotrebno zbog beskonačne petlje)
     pthread_barrier_destroy(&barrier);
 
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) 
+            pthread_join(threads[i][j], NULL);
+    }
+    
     return 0;
 }
 
